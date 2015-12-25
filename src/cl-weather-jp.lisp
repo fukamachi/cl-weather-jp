a8e002147b1b84ec7556f2b4669eebeab15cfd45
Eitaro Fukamachi <e.arrows@gmail.com>
2015-12-25 13:57:24 +0900

    Add city-forecast-detail-url.

--- src/cl-weather-jp.lisp
+++ src/cl-weather-jp.lisp
@@ -5,9 +5,13 @@
         #:cl-weather-jp.error
         #:cl-weather-jp.util)
   (:export #:city-forecasts
+           #:city-forecast-detail-url
            #:unknown-location))
 (in-package :cl-weather-jp)
 
 (defun city-forecasts (city)
   (let ((info (retrieve-city-weather city)))
     (values (aget info "forecasts"))))
+
+(defun city-forecast-detail-url (city)
+  (values (aget (retrieve-city-weather city) "link")))
--- t/cl-weather-jp.lisp
+++ t/cl-weather-jp.lisp
@@ -5,15 +5,22 @@
         :prove))
 (in-package :cl-weather-jp-test)
 
-(plan 3)
+(plan 2)
 
-(ok (city-forecasts "東京")
-    "Can retrieve forecasts for 東京")
+(subtest "city-forecasts"
+  (ok (city-forecasts "東京")
+      "Can retrieve forecasts for 東京")
 
-(ok (city-forecasts "福島")
-    "Can retrieve forecasts for 福島")
+  (ok (city-forecasts "福島")
+      "Can retrieve forecasts for 福島")
 
-(is-error (city-forecasts "ホーチミン")
-          'unknown-location)
+  (is-error (city-forecasts "ホーチミン")
+            'unknown-location))
+
+(subtest "city-forecast-detail-url"
+  (ok (city-forecast-detail-url "東京"))
+  (ok (city-forecast-detail-url "福島"))
+  (is-error (city-forecast-detail-url "オワフ島")
+            'unknown-location))
 
 (finalize)
